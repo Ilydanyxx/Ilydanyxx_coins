f3a40ed6391292f4024b292e5dc25249aa822411
Ilydanyxx <danya.chuyko@icloud.com>
2025-04-26 03:49:18 +0300

    оновлено

--- app.js
+++ app.js
@@ -9,26 +9,33 @@ const orderRoutes = require('./project-root/routes/orders');
 
 dotenv.config();
 const app = express();
+
+// Додаємо статичну папку для обробки файлів з views
+app.use(express.static('views'));
+app.use(express.static('public'));
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+
 console.log(process.env.MONGO_URI);
+
 // Підключення до бази даних
 mongoose.connect(process.env.MONGO_URI)
-
   .then(() => console.log('MongoDB connected'))
   .catch((err) => {
     console.error('MongoDB connection error:', err);
   });
-
-
-
 
 // Роутинг
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+
+// Роут для адмін-сторінки
+app.get('/admin.html', (req, res) => {
+  res.sendFile(__dirname + '/views/admin.html');
+});
 
 // Запуск сервера
 app.listen(process.env.PORT, () => {
