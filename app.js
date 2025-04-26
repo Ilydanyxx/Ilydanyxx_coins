8f036e1e4e5abcf5cfdac3ff8042f3203c2c2229
Ilydanyxx <danya.chuyko@icloud.com>
2025-04-26 04:05:01 +0300

    оновлено

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 app.get('/index.html', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/project-root/views/index.html');
 });
 
 app.get('/login.html', (req, res) => {
